0c4477680e48a863ec4bfba52188a28b4bd870eb
Jeffrey Varughese <jeffvarughese@gmail.com>
2023-06-13 23:17:00 -0500

    completed assignment

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,3 +1,44 @@
 console.log("Hello World!");
 
-let x = 1
+
+
+
+// coding along with class //
+
+let x = 1;
+
+let tempRightNow = 81
+
+console.log("temp before too much Diablo" , tempRightNow);
+
+tempRightNow = tempRightNow + 1;
+
+tempRightNow += 5;
+
+tempRightNow ++;
+
+console.log("temp after too much Diablo" , tempRightNow);
+
+let tempLater = '<p class="container">cool</p>'
+let game = "Zelda";
+let title = "TotK";
+
+game = game + ' ' + title;
+
+console.log('game' , game);
+
+//booleans
+
+let isCloudy = false;
+
+//let toReplace = document.querySelector('#fun2');
+
+//toReplace.innerHTML = tempLater;
+
+//Conditionals
+
+if (isCloudy === false) {
+    console.log('clear skies');
+} else {
+    console.log('rainy day');
+}
